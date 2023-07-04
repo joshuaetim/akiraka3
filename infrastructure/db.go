e633dd0c4fe62808755eb29e021d53cf0c97ffb4
Joshua Etim <jetimworks@gmail.com>
2023-07-04 12:24:07 +0100

    url db

--- infrastructure/db.go
+++ infrastructure/db.go
@@ -25,6 +25,9 @@ func DB() *gorm.DB {
 		}
 	case "mysql":
 		dsn = fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", os.Getenv("DB_USER"), os.Getenv("DB_PASS"), os.Getenv("DB_HOST"), os.Getenv("DB_DATABASE"))
+		if os.Getenv("DB_URL") != "" {
+			dsn = os.Getenv("DB_URL")
+		}
 		dialect = mysql.Open(dsn)
 	default:
 		log.Fatalf("invalid driver: %s", os.Getenv("DB_DRIVER"))
